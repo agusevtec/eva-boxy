be6ce1d3434680241623830c31bd1813f62b328e
agusevtec <agusevtec@gmail.com>
2026-06-04 17:16:19 +0300

    import new implementation of pcd8544

--- src/evabScreenPCD8544.cpp
+++ src/evabScreenPCD8544.cpp
@@ -1,102 +1,110 @@
 #include <evabScreenPCD8544.h>
 
-using namespace evab;
-
-ScreenPCD8544::ScreenPCD8544(const IFont *aFont, uint8_t aDC, uint8_t aCS, uint8_t aRST, uint8_t aLED)
-    : ScreenPage8Base(aFont), mDCPin(aDC), mCSPin(aCS), mRSTPin(aRST), mLEDPin(aLED)
+namespace evab
 {
-    pinMode(mDCPin, OUTPUT);
-    pinMode(mCSPin, OUTPUT);
+
+  ScreenPCD8544::ScreenPCD8544(const IFont *aFont, 
+                               uint8_t aRST, uint8_t aCE, uint8_t aDC, 
+                               uint8_t aDIN, uint8_t aCLK,
+                               uint8_t aLED)
+    : ScreenPage8Base(aFont), mRSTPin(aRST), mCEPin(aCE), mDCPin(aDC), mLEDPin(aLED)
+  {
     pinMode(mRSTPin, OUTPUT);
-
+    pinMode(mCEPin, OUTPUT);
+    pinMode(mDCPin, OUTPUT);
+    
     if (mLEDPin != 255)
-        pinMode(mLEDPin, OUTPUT);
-
-    digitalWrite(mCSPin, HIGH);
-
+      pinMode(mLEDPin, OUTPUT);
+    
+    digitalWrite(mCEPin, HIGH);
+    
+    // Аппаратный сброс
     digitalWrite(mRSTPin, LOW);
     delay(10);
     digitalWrite(mRSTPin, HIGH);
     delay(10);
-
+    
     SPI.begin();
     SPI.setBitOrder(MSBFIRST);
     SPI.setDataMode(SPI_MODE0);
     SPI.setClockDivider(SPI_CLOCK_DIV4);
-
+    
     initDisplay();
     clear();
-}
+  }
 
-ScreenPCD8544::~ScreenPCD8544()
-{
-}
+  ScreenPCD8544::~ScreenPCD8544()
+  {
+  }
 
-void ScreenPCD8544::setBacklight(uint8_t aState)
-{
+  void ScreenPCD8544::setBacklight(uint8_t aState)
+  {
     if (mLEDPin != 255)
-        digitalWrite(mLEDPin, aState);
-}
+      digitalWrite(mLEDPin, aState);
+  }
 
-void ScreenPCD8544::SetContrast(uint8_t aContrast)
-{
+  void ScreenPCD8544::setContrast(uint8_t aContrast)
+  {
     sendCommand(CMD_FUNC_SET | 0x01);
     sendCommand(CMD_SET_VOP | (aContrast & 0x7F));
     sendCommand(CMD_FUNC_SET);
-}
+  }
 
-void ScreenPCD8544::clear()
-{
+  void ScreenPCD8544::clear()
+  {
     for (uint8_t page = 0; page < 6; page++)
     {
-        for (uint8_t col = 0; col < 84; col++)
-        {
-            DrawVerticalSlice({(uint8_t)(col / 8), page}, col % 8, 0x00);
-        }
+      for (uint8_t col = 0; col < 84; col++)
+      {
+        DrawVerticalSlice({(uint8_t)(col / 8), page}, col % 8, 0x00);
+      }
     }
-}
+  }
 
-Coor ScreenPCD8544::Size()
-{
-    return {11, 6};
-}
-
-void ScreenPCD8544::DrawVerticalSlice(Coor aPosition, unsigned char aSliceColumn, unsigned char aSlice)
-{
-    uint8_t x = aPosition.X * 8 + aSliceColumn;
-    if (x >= 84 || aPosition.Y >= 6)
-        return;
-
-    sendCommand(CMD_SET_Y | (aPosition.Y & 0x07));
-    sendCommand(CMD_SET_X | (x & 0x7F));
-
-    digitalWrite(mDCPin, HIGH);
-    digitalWrite(mCSPin, LOW);
-    SPI.transfer(aSlice);
-    digitalWrite(mCSPin, HIGH);
-}
-
-void ScreenPCD8544::ClearTile(Coor aPosition, unsigned char aColor)
-{
-    for (uint8_t col = 0; col < 8; col++)
-    {
-        DrawVerticalSlice({aPosition.X, aPosition.Y}, col, aColor ? 0xFF : 0x00);
-    }
-}
-
-void ScreenPCD8544::sendCommand(uint8_t aCmd)
-{
+  void ScreenPCD8544::sendCommand(uint8_t aCmd)
+  {
     digitalWrite(mDCPin, LOW);
-    digitalWrite(mCSPin, LOW);
+    digitalWrite(mCEPin, LOW);
     SPI.transfer(aCmd);
-    digitalWrite(mCSPin, HIGH);
-}
+    digitalWrite(mCEPin, HIGH);
+  }
 
-void ScreenPCD8544::initDisplay()
-{
+  void ScreenPCD8544::sendData(uint8_t aData)
+  {
+    digitalWrite(mDCPin, HIGH);
+    digitalWrite(mCEPin, LOW);
+    SPI.transfer(aData);
+    digitalWrite(mCEPin, HIGH);
+  }
+
+  void ScreenPCD8544::initDisplay()
+  {
     sendCommand(CMD_FUNC_SET);
     sendCommand(CMD_DISP_CTRL | 0x04);
     sendCommand(CMD_SET_Y | 0x00);
     sendCommand(CMD_SET_X | 0x00);
-    SetContrast(0x30);
+    setContrast(0x30);
+  }
+
+  void ScreenPCD8544::DrawVerticalSlice(Coor aPosition, unsigned char aSliceColumn, unsigned char aSlice)
+  {
+    uint8_t x = aPosition.X * 8 + aSliceColumn;
+    uint8_t page = aPosition.Y;
+    
+    if (x >= 84 || page >= 6)
+      return;
+    
+    sendCommand(CMD_SET_Y | (page & 0x07));
+    sendCommand(CMD_SET_X | (x & 0x7F));
+    sendData(aSlice);
+  }
+
+  void ScreenPCD8544::ClearTile(Coor aPosition, unsigned char aColor)
+  {
+    for (uint8_t col = 0; col < 8; col++)
+    {
+      DrawVerticalSlice({aPosition.X, aPosition.Y}, col, aColor ? 0xFF : 0x00);
+    }
+  }
+
 }
--- src/evabScreenPCD8544.h
+++ src/evabScreenPCD8544.h
@@ -1,41 +1,45 @@
 #pragma once
 
 #include <evabScreenPage8Base.h>
-#include <evabIFont.h>
 #include <SPI.h>
 
 namespace evab
 {
 
-    class ScreenPCD8544 : public ScreenPage8Base
-    {
-    public:
-        ScreenPCD8544(const IFont *aFont, uint8_t aDC, uint8_t aCS, uint8_t aRST, uint8_t aLED = 255);
-        ~ScreenPCD8544();
+  class ScreenPCD8544 : public ScreenPage8Base
+  {
+  public:
+    ScreenPCD8544(const IFont *aFont, 
+                  uint8_t aRST, uint8_t aCE, uint8_t aDC, 
+                  uint8_t aDIN, uint8_t aCLK,
+                  uint8_t aLED = 255);
+    ~ScreenPCD8544();
+    
+    void setBacklight(uint8_t aState);
+    void setContrast(uint8_t aContrast);
+    void clear();
+    Coor Size() override { return {11, 6}; }
 
-        void setBacklight(uint8_t aState);
-        void SetContrast(uint8_t aContrast);
-        void clear();
-        Coor Size() override;
+  protected:
+    void DrawVerticalSlice(Coor aPosition, unsigned char aSliceColumn, unsigned char aSlice) override;
+    void ClearTile(Coor aPosition, unsigned char aColor) override;
 
-    protected:
-        void DrawVerticalSlice(Coor aPosition, unsigned char aSliceColumn, unsigned char aSlice) override;
-        void ClearTile(Coor aPosition, unsigned char aColor) override;
-
-    private:
-        void sendCommand(uint8_t aCmd);
-        void initDisplay();
-
-        static const uint8_t CMD_FUNC_SET = 0x20;
-        static const uint8_t CMD_DISP_CTRL = 0x08;
-        static const uint8_t CMD_SET_Y = 0x40;
-        static const uint8_t CMD_SET_X = 0x80;
-        static const uint8_t CMD_SET_VOP = 0x80;
-
-        uint8_t mDCPin;
-        uint8_t mCSPin;
-        uint8_t mRSTPin;
-        uint8_t mLEDPin;
-    };
+  private:
+    void sendCommand(uint8_t aCmd);
+    void sendData(uint8_t aData);
+    void initDisplay();
+    
+    static const uint8_t CMD_FUNC_SET   = 0x20;
+    static const uint8_t CMD_DISP_CTRL  = 0x08;
+    static const uint8_t CMD_SET_Y      = 0x40;
+    static const uint8_t CMD_SET_X      = 0x80;
+    static const uint8_t CMD_SET_VOP    = 0x80;
+    
+    uint8_t mRSTPin;
+    uint8_t mCEPin;
+    uint8_t mDCPin;
+    uint8_t mLEDPin;
+  };
 
 }
+
